7c1bb1bc83c9401ec3e31a6f3b60a1097989c6e9
K-Saikrishnan <53394202+K-Saikrishnan@users.noreply.github.com>
2025-06-08 19:04:06 +0530

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# utils
+
+[![CI](https://github.com/K-Saikrishnan/utils/actions/workflows/ci.yaml/badge.svg)](https://github.com/K-Saikrishnan/utils/actions/workflows/ci.yaml)
---
+++ eslint.config.js
@@ -0,0 +1,47 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const eslintConfigPrettier = require('eslint-config-prettier/flat');
+const stylistic = require('@stylistic/eslint-plugin');
+
+module.exports = tseslint.config(
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+      stylistic.configs.recommended,
+      eslintConfigPrettier,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      curly: 2,
+      '@stylistic/lines-between-class-members': ['error', 'always'],
+      '@stylistic/line-comment-position': ['error', { position: 'above' }],
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
+    rules: {},
+  },
+);
